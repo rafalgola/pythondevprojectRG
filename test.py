b3301ad4dc695957d59d1c19fa2fbfb0a347b139
Rafal Gola <rafal.gola@protonmail.com>
2024-05-21 09:16:11 +0200

    timdelta inc. to 3 @ test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def was_db_modified_within_last_minute(file_path):
     last_modified_time = os.path.getmtime(file_path)
     last_modified_date = datetime.fromtimestamp(last_modified_time)
     current_time = datetime.now()
-    return (current_time - last_modified_date) <= timedelta(minutes=1)
+    return (current_time - last_modified_date) <= timedelta(minutes=3)
 
 
 class TestFileModification(unittest.TestCase):
